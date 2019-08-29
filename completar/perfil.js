da9db5f0d077d44dc11fd41dd7440bec5e45a1b1
Karen1311 <hoc.karenburdman@hotmail.com>
2019-08-29 12:42:11 -0300

    Actualización

--- completar/perfil.js
+++ completar/perfil.js
@@ -3,17 +3,59 @@ const descripcionInput = document.getElementById('descripcion-input');
 
 const actualizarBusquedas = () => {
     listaBusquedas.innerHTML = '';
-    // agregarBusqueda(id, categoria, descripcion)
+    //agregarBusqueda(data.id, data.categoria, data.descripcion)
+    fetch(`http://localhost:3000/busquedas`)
+    .then(respuesta => respuesta.json())
+    .then(busquedas => {
+        for(const busqueda of busquedas){
+            agregarBusqueda(busqueda.id, busqueda.categoria, busqueda.descripcion)
+        }
+    });
 }
 
 const crearBusqueda = () => {
-
-}
+    fetch(`http://localhost:3000/busquedas`, {
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            categoria: categoriaSelect.value,
+            descripcion: descripcionInput.value
+        })
+    })
+    .then(respuesta => respuesta.json())
+    .then(data => {
+        actualizarBusquedas();
+        ocultarModal();
+    });
+};
 
 const editarBusqueda = id => {
+    fetch(`http://localhost:3000/busquedas/${id}`, {
+    method: 'put',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            categoria: categoriaSelect.Value,
+            descripcion: descripcionInput.value
+        })
+    })
+        .then(respuesta => respuesta.json())
+        .then(data => {
+        actualizarBusquedas();
+        ocultarModal();
+    });
 }
 
+
 const eliminarBusqueda = id => {
+    fetch(`http:localhost:3000/busquedas/${id}`,{
+        method: 'delete',
+    })
+    .then(respuesta => respuesta.json())
+    .then(data => actualizarBusquedas());
 }
 
 const actualizarOfertas = () => {
@@ -33,3 +75,53 @@ const eliminarOferta = id => {
 actualizarCategorias(['Electricidad', 'Plomería', 'Albañilería', 'Gas']);
 actualizarBusquedas();
 actualizarOfertas();
+
+// const obtenerPeliculas = () => {
+//     fetch('http://localhost:3000/peliculas')
+//     .then(respuesta => respuesta.json())
+//     .then(data => console.log(data));
+// }
+
+// const obtenerPelicula = (id) => {
+//     fetch(`http://localhost:3000/peliculas/${id}`)
+//     .then(respuesta => respuesta.json())
+//     .then(data => console.log(data));
+// }
+
+// const agregarPelicula = nombrePelicula => {
+//     fetch('http://localhost:3000/peliculas',{
+//         method: 'post',
+//         headers:{
+//             'Content-type':'application/json'
+//         },
+//         body: JSON.stringify({
+//             nombre: nombrePelicula
+//         })
+//     })
+//     .then(respuesta => respuesta.json())
+//     .then(data => console.log(data));
+// } 
+
+// const eliminarPelicula = id => {
+//     fetch(`http://localhost:3000/peliculas/${id}`,{
+//         method: 'delete',
+//     })
+//     .then(respuesta => respuesta.json())
+//     .then(data => console.log(data));
+// } 
+
+// const actualizarPelicula = (id, nombrePelicula) => {
+//     fetch(`http://localhost:3000/peliculas/${id}`,{
+//         method: 'put',
+//         headers:{
+//             'Content-type':'application/json'
+//         },
+//         body: JSON.stringify({
+//             nombre: nombrePelicula
+//         })
+//     })
+//     .then(respuesta => respuesta.json())
+//     .then(data => console.log(data));
+// } 
+
+
